0cad1002278e1590dda6d0a71b4fcb5dc602adf1
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-10 15:19:39 +0530

    add routing and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { Signup } from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
