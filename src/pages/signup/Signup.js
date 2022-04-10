c7d6d512757ccd9a90ac4ec2f512fe4a9e4a93bd
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-10 15:46:52 +0530

    make signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Signup.module.css'
 
 export const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password, displayName);
+    }
+
     return (
-        <div>Signup</div>
+        <form onSubmit={handleSubmit} className={styles['signup-form']}>
+            <h2>Signup</h2>
+            <label>
+                <span>Display name:</span>
+                <input type="text" onChange={(e) => setDisplayName(e.target.value)} value={displayName} />
+            </label>
+            <label>
+                <span>email:</span>
+                <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+            </label>
+            <label>
+                <span>password:</span>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+            </label>
+            <button className='btn'>Signup</button>
+        </form>
     )
 }
