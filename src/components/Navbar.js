5a2298de708b56cb1c6601fea9e466c5f8d6084e
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-11 17:54:14 +0530

    conditionally show content in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 function Navbar() {
     const { logout } = useLogout()
+    const { user } = useAuthContext()
+
     return (
         <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>myMoney</li>
+                {!user && (
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="signup">Signup</Link></li>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <li>hello, {user.displayName}</li>
+                        <li><button className='btn' onClick={logout}>Logout</button></li>
+                    </>
+                )}
 
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="signup">Signup</Link></li>
-
-                <li><button className='btn' onClick={logout}>Logout</button></li>
             </ul>
         </nav>
     )
