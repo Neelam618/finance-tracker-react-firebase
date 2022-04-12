c77b234186dc70c9f58dd9e31762911a71109d40
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-12 11:28:07 +0530

    redirect pages depending on user login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import { Signup } from './pages/signup/Signup';
@@ -6,16 +6,16 @@ import Navbar from './components/Navbar'
 import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
-  const { authIsReady } = useAuthContext()
+  const { authIsReady, user } = useAuthContext()
   return (
     <div className="App">
       {authIsReady && (
         <Router>
           <Navbar />
           <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route exact path="/" element={(user && <Home />) || (!user && <Navigate to="/login" />)} />
+            <Route path="/login" element={(!user && <Login />) || (user && <Navigate to="/" />)} />
+            <Route path="/signup" element={(!user && <Signup />) || (user && <Navigate to="/" />)} />
             {/* <Route path="*" element={<NotFound />} /> */}
           </Routes>
         </Router>
