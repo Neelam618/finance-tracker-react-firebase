4b27ca445447a474e15b64e68038999a0959635e
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-12 11:57:42 +0530

    create useFirestore hook

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,40 @@
+import { useReducer, useState, useEffect } from 'react'
+import { projectFirestore } from "../firebase/config"
+
+let initialState = {
+    document: null,
+    isPendingState: false,
+    error: null,
+    success: null
+}
+
+const firestoreReducer = (state, action) => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+export const useFireStore = (collection) => {          //transactions
+    const [response, dispatch] = useReducer(firestoreReducer, initialState)
+    const [isCancelled, setIsCancelled] = useState(false)
+
+    //collection ref
+    const ref = projectFirestore.collection(collection)
+
+    //add a document
+    const addDocument = (doc) => {
+
+    }
+
+    const deleteDocument = (id) => {
+
+    }
+
+    useEffect(() => {
+        return () => setIsCancelled(true)
+    }, [])
+
+    return { addDocument, deleteDocument, response }
+}
